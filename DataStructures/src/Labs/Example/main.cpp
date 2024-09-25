0d598e607c1c56f797b6e9427d16cd1cb3fdbfad
Sam DePoule <samdepoule@gmail.com>
2024-09-24 17:56:01 -0700

    New datascrtuctures folder

---
+++ DataStructures/src/Labs/Example/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+
+    // To read a single word, you can use the cin command
+    std::cout << "Hello World!" << std::endl;
+
+    return 0;
+}
---
+++ DataStructures/src/Labs/Time Conversion/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    // To read a single word, you can use the cin command
+    cout << "Please enter a number of seconds:" << endl;
+    int inputSeconds;
+    cin >> inputSeconds;
+
+    int seconds = inputSeconds;
+
+    constexpr int secondsInMinute = 60;
+    constexpr int secondsInHour = secondsInMinute * 60;
+    constexpr int secondsInDay = secondsInHour * 24;
+
+    const int days = seconds / secondsInDay;
+    seconds -= days * secondsInDay;
+    const int hours = seconds / secondsInHour;
+    seconds -= hours * secondsInHour;
+    const int minutes = seconds / secondsInMinute;
+    seconds -= minutes * secondsInMinute;
+
+    // Example: 3,661 second(s) is equivalent to 0 day(s), 1 hour(s), 1 minute(s), and 1 second(s).
+    cout << inputSeconds << " second(s) is equivalent to " << days << " day(s), " << hours << " hour(s), " << minutes << " minute(s), and " << seconds << " second(s)." << endl;
+
+    return 0;
+}
